2994806dcdcbfcdddfaf15712419c6e939b231d3
Yu Liu <liuyuc@outlook.com>
2017-02-12 10:23:00 -0500

    problem1 finished

--- proj2.py
+++ proj2.py
@@ -1,4 +1,12 @@
 #proj2.py
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 
 #### Problem 1 ####
@@ -6,6 +14,20 @@ print('\n*********** PROBLEM 1 ***********')
 print('New York Times -- First 10 Story Headings\n')
 
 ### Your Problem 1 solution goes here
+url = "http://nytimes.com"
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+head_title = soup.find_all(class_="story-heading")
+
+print_title = []
+for item in head_title:
+    if item.a:
+        print_title.append(item.a.text.replace("\n", " ").strip())
+    else:
+        print_title.append(item.contents[0].strip())
+
+for title in print_title[:10]:
+    print (title)
 
 
 #### Problem 2 ####
